37f37b756ed1fc9f4e61a2ce9c949ef49e234df7
Peter deHaan <peter@deseloper.com>
2016-08-26 16:52:09 -0700

    Refactor some bits and add CLI (#3)

--- lib.js
+++ lib.js
@@ -2,6 +2,8 @@ const url = require('url');
 
 const fetch = require('node-fetch');
 const getPackageJson = require('get-package-json');
+const gitUrlParse = require('git-url-parse');
+const sortJson = require('sort-json');
 
 function scraper(moduleName, limit=500) {
   const dependedUponUrl = url.format({
@@ -19,32 +21,44 @@ function scraper(moduleName, limit=500) {
     .then((res) => res.json())
     .then(({rows}) => {
       const dependents = rows.reduce((prev, {key:[mod, name, desc]}) => {
-        prev.push({name, desc});
+        prev.push(name);
         return prev;
       }, []);
-      return {
-        name: moduleName,
-        dependents
-      };
+      return Promise.all(dependents.map(getLatestPackageJson));
+    })
+    .then((dependents) => {
+      // Sort all the modules by last published date descending (newest on top).
+      const sortModifiedDesc = (pkgA, pkgB) => new Date(pkgB.modified) - new Date(pkgA.modified);
+      return dependents.sort(sortModifiedDesc);
     });
 }
 
 function getLatestPackageJson(name) {
   return getPackageJson(name)
     .then(({pkg}) => {
-      const latest = pkg['dist-tags'].latest;
-      pkg.versions[latest].modified = new Date(pkg.time.modified);
-      return pkg.versions[latest];
+      const latestVer = pkg['dist-tags'].latest;
+      const latestPkg = pkg.versions[latestVer];
+      latestPkg.modified = pkg.time.modified; // inject modified date.
+      return latestPkg;
     })
     .then(({name, version, repository=null, dependencies={}, devDependencies={}, modified}) => {
-      return {
+      const fixRepository = (repo) => {
+        if (repo && repo.url) {
+          return gitUrlParse(repo.url).toString('https') || repo.url;
+        }
+        // Something is wrong with the repository in package.json. Give up.
+        return null;
+      };
+      repository = fixRepository(repository);
+
+      return sortJson({
         name,
         version,
         repository,
         modified,
-        // dependencies,
-        // devDependencies
-      }
+        dependencies,
+        devDependencies
+      })
     });
 }
 
